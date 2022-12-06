ff1b227078b08fc47073bfc8d598ea0c153dd65a
keval-pixel <keval.visnu@tecnico.ulisboa.pt>
2022-12-06 23:40:44 +0000

    added tables for country, boat, certificate and valid_for

--- project_2/schema.sql
+++ project_2/schema.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS Location,Trip,Reservation,
-    Boat,Date_interval,Country;
+    Boat,Date_interval,Country, Boat_Class, Sailing_Certificate CASCADE ;
 
 CREATE TABLE Boat(
     boat_cni VARCHAR(80),
@@ -47,4 +47,44 @@ CREATE TABLE Trip(
                                date_inter_end)
 
 --A boat can not take off on a trip before the reservation start date.
+);
+
+CREATE TABLE Country(
+    name VARCHAR(80),
+    flag VARCHAR(80) NOT NULL,
+    ISO_code INTEGER,
+    PRIMARY KEY(name),
+    UNIQUE(flag),
+    UNIQUE(ISO_code)
+);
+
+CREATE TABLE Boat_Class(
+    name VARCHAR(80),
+    max_length NUMERIC(12,4) NOT NULL,
+    PRIMARY KEY(name)
+);
+
+CREATE TABLE Sailing_Certificate(
+    boat_name VARCHAR(80) NOT NULL,
+    email VARCHAR(80) NOT NULL,
+    issue_date DATE NOT NULL,
+    expiry_date DATE NOT NULL,
+    PRIMARY KEY(email, issue_date),
+    --uncomment when sailor table is done
+    --FOREIGN KEY(email) REFERENCES Sailor(email),
+    FOREIGN KEY(boat_name) REFERENCES Boat_Class(name),
+    UNIQUE(issue_date)
+    --every certificate must exist in the table valid_for
+);
+
+--There is an error in this table
+CREATE TABLE valid_for(
+    country_name VARCHAR(80) NOT NULL,
+    issue_date DATE NOT NULL,
+    boat_name VARCHAR(80) NOT NULL,
+    PRIMARY KEY(country_name, issue_date, boat_name),
+    FOREIGN KEY(country_name) REFERENCES Country(name),
+    FOREIGN KEY(issue_date) REFERENCES Sailing_Certificate(issue_date),
+    FOREIGN KEY(boat_name) REFERENCES Sailing_Certificate(boat_name),
+    UNIQUE(country_name)
 );
